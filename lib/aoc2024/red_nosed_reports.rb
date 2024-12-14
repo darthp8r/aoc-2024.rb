a3853ef09a8d24ee64ada2490c4fad790109fa3c
darthp8r <darthp8r@gmail.com>
2024-12-13 21:24:00 -0500

    when a digit flips, sometimes it's the one before it that needs to go

--- lib/aoc2024/red_nosed_reports.rb
+++ lib/aoc2024/red_nosed_reports.rb
@@ -9,50 +9,101 @@ module Aoc2024
       end
     end
 
-    def safety_dance
-      @reports.inject(0) do |memo, item|
-        if safe? item
-          memo + 1
+    # determine increasing or decreasing trend by summing identities for each pair
+    #
+    def self.direction levels
+      where = levels[1..-1].inject([levels[0],0]) do |memo, item|
+        previous, accumulator = *memo
+        if previous < item
+          [item, accumulator+1]
+        elsif item < previous
+          [item, accumulator-1]
         else
-          memo
+          [item, accumulator]
         end
+      end[1]
+
+      if where < 0
+        :decreasing
+      elsif 0 < where
+        :increasing
+      else
+        :flatlined
       end
     end
 
-    private def safe? dancers
-      # determine the flow direction
-    # pp dancers
-      if dancers[0] < dancers[1]
-      # pp ""
-      # pp "SET INCREASING"
-        trend = :increasing
-      elsif dancers[1] < dancers[0]
-      # pp ""
-      # pp "SET DECREASING"
-        trend = :decreasing
-      else
-        return false
+    def safety_dance
+      @reports.inject([]) do |memo, item|
+        memo.append item if safe? item
+        memo
       end
+    end
 
-      dancers[1..-1].inject(dancers[0]) do |a, b|
-      # pp "compare #{a} with #{b}"
-        if a == b
-      #   pp "EQUALS #{b}"
+    private def safe? levels
+      # determine the flow direction
+      trend = Rednosedreports.direction levels
+      return false if trend == :flatlined
+
+      levels[1..-1].inject(levels[0]) do |memo, item|
+        if memo == item
           return false
-        elsif 3 < (a-b).abs
-      #   pp "DIVERGENT #{a} #{b}"
+        elsif 3 < (memo-item).abs
           return false
-        elsif (trend == :increasing) and (a < b)
-          b
-        elsif (trend == :decreasing) and (b < a)
-          b
+        elsif not consistent?(trend, memo, item)
+          return false
         else
-      #   pp "CHANGED DIRECTIONS #{a} #{b}"
-          return false
+          item
         end
       end
-    # pp "SAFE!"
       true
+    end
+
+    def within_tolerance
+      @reports.inject([]) do |memo, item| 
+        if safe? item
+          memo.append item
+        elsif safe_enough? item
+          memo.append item
+        else
+        end
+      # memo.append item if ((safe? item) or (safe_enough? item))
+        memo
+      end
+    end
+
+    # 419 is too high
+    # 399 is too low
+    private def safe_enough? levels
+
+      # determine the flow direction
+      trend = Rednosedreports.direction levels
+      return false if trend == :flatlined
+
+      delete_me = offender(trend, levels)
+      if delete_me
+        safe?(levels[0...delete_me] + levels[1+delete_me..-1]) or
+          safe?(levels[0...delete_me-1] + levels[delete_me..-1])
+      else
+        true
+      end
+    end
+
+    private def offender(trend, levels)
+      (0...(levels.size-1)).each do |index|
+        a, b = levels[index], levels[index+1]
+        if a == b
+          return index+1
+        elsif 3 < (a-b).abs
+          return index+1
+        elsif not consistent?(trend, a, b)
+          return index+1
+        end
+      end
+      nil
+    end
+
+    private def consistent?(direction, a, b)
+      ((direction == :increasing) and (a < b)) or ((direction == :decreasing) and (b < a))
     end
 
   end
--- spec/aoc2024/day_02_spec.rb
+++ spec/aoc2024/day_02_spec.rb
@@ -1,5 +1,37 @@
 module Aoc2024
   RSpec.describe Rednosedreports do
+    context "Prep Work" do
+      describe "Increasing Trends" do
+        it "agrees with a normal progression" do
+          expect(Rednosedreports.direction([1,2,3,4,5])).to be :increasing
+        end
+
+        it "agrees with a flipped progression" do
+          expect(Rednosedreports.direction([1,3,2,4,5])).to be :increasing
+        end
+      end
+
+      describe "Decreasing Trends" do
+        it "agrees with a normal progression" do
+          expect(Rednosedreports.direction([1,2,3,4,5].reverse)).to be :decreasing
+        end
+
+        it "agrees with a flipped progression" do
+          expect(Rednosedreports.direction([1,3,2,4,5].reverse)).to be :decreasing
+        end
+      end
+
+      describe "Undeterminable Trends" do
+        it "down-up-down-up is flatlined" do
+          expect(Rednosedreports.direction([2,1,4,3,6].reverse)).to be :flatlined
+        end
+
+        it "up-up-down-down is flatlined" do
+          expect(Rednosedreports.direction([1,2,4,3,2].reverse)).to be :flatlined
+        end
+      end
+    end
+
     context "Sample" do
       before(:all) do
         @subject = Rednosedreports.new Support.file_to_list __FILE__.gsub('spec.rb', 'sample.txt')
@@ -8,17 +40,48 @@ module Aoc2024
 
       describe "Part 1" do
         it "dunsels" do
-          pp subject.safety_dance
         end
 
         it "sample data agrees" do
-          expect(subject.safety_dance).to eq 2
+          expect(subject.safety_dance.count).to eq 2
         end
       end
 
       describe "Part 2" do
-        it "sample data agrees" ,:skip do
-          expect(subject.part_deux).to eq 2
+        it "sample data agrees" do
+          expect(subject.within_tolerance.count).to eq 4
+        end
+      end
+    end
+
+    # maybe do the safe? or safe_enough? where safe_enough? knows there's a problem,
+    # scoots immediately to the (first) offender, and then runs safe? on the report
+    # minus that one element. O(2n)
+    #
+    context "Fabricated" do
+      before(:all) do
+        @subject = Rednosedreports.new [
+          "1 2 3 8 4",  # SAFE w/o 3
+          "1 2 3 4 5",  # SAFE
+          "1 2 3 3 4",  # SAFE w/o 3
+          "1 1 3 3 5",  # FAIL copied
+          "1 2 9 3 5",  # SAFE w/o 2
+          "7 5 4 2 1",  # SAFE
+        ]
+      end
+      subject { @subject }
+
+      describe "Part 3/2" do
+      # it "dunsels" do
+      #   pp subject.reports
+      # end
+
+        it "actual answer above" do
+          pp subject.within_tolerance.count
+        end
+
+        it "results survive" do
+          expect(subject.within_tolerance.count).to eq 5
         end
       end
     end
@@ -31,21 +94,21 @@ module Aoc2024
 
       describe "Part 1" do
         it "actual answer above" do
-          pp subject.safety_dance
-        end
-
-        it "results survive", :skip do
-          expect(subject.part_one).to eq 1111
-        end
-      end
-
-      describe "Part 2" ,:skip do
-        it "actual answer above" do
-          pp subject.part_deux
+          pp subject.safety_dance.count
         end
 
         it "results survive" do
-          expect(subject.part_deux).to eq 2222
+          expect(subject.safety_dance.count).to eq 356
+        end
+      end
+
+      describe "Part 2" do
+        it "actual answer above" do
+          pp subject.within_tolerance.count
+        end
+
+        it "results survive" do
+          expect(subject.within_tolerance.count).to eq 413
         end
       end
     end
